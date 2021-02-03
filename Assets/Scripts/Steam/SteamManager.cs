648fbe9d609c49ae3d12db2ece1e4ad95b8c8df8
NotLimey <martin@kulvis.no>
2021-02-03 22:27:45 +0100

    Steamworks

---
+++ Assets/Scripts/Steam/SteamManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+   private void Awake()
+    {
+        try
+        {
+            Steamworks.SteamClient.Init(1541950);
+        }
+        catch
+        {
+            Debug.Log("Couldn't Initialize Steam Client");
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+   private void OnDisable()
+   {
+       Steamworks.SteamClient.Shutdown();
+   }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Steamworks.SteamClient.RunCallbacks();
+    }
+}
